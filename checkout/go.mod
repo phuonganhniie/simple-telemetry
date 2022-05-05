292668d3742d62c59a14aa8b144d49d0d1c64ff0
phuonganhniie <anhdp@hanet.com>
2022-05-05 10:26:22 +0700

    define checkout grpc server & tracing span + event

--- checkout/go.mod
+++ checkout/go.mod
@@ -3,11 +3,24 @@ module github.com/phuonganhniie/simpleTelemetry/checkout
 go 1.17
 
 require (
+	github.com/phuonganhniie/simpleTelemetry/proto v0.0.0-20220504163505-15e39c5ca386
+	github.com/phuonganhniie/simpleTelemetry/utils v0.0.0-20220504163505-15e39c5ca386
+	github.com/streadway/amqp v1.0.0
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.32.0
+	go.opentelemetry.io/otel v1.7.0
+	go.opentelemetry.io/otel/trace v1.7.0
+	google.golang.org/grpc v1.46.0
+)
+
+require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	go.opentelemetry.io/otel v1.7.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	go.opentelemetry.io/otel/exporters/jaeger v1.7.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.7.0 // indirect
-	go.opentelemetry.io/otel/trace v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- checkout/main.go
+++ checkout/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net"
 
+	pb_checkout "github.com/phuonganhniie/simpleTelemetry/proto"
 	"github.com/phuonganhniie/simpleTelemetry/utils"
+	"github.com/streadway/amqp"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -21,6 +30,52 @@ func main() {
 		log.Fatalf("failed to create tracer: %v", err)
 	}
 
-	channel, closeConn := utils.ConnectAmqp(amqpUser, amqpPass, amqpHost, amqpPort)
+	ch, closeConn := utils.ConnectAmqp(amqpUser, amqpPass, amqpHost, amqpPort)
 	defer closeConn()
+
+	lis, err := net.Listen("tcp", grpcAddress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor()),
+		grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()))
+
+	pb_checkout.RegisterCheckoutServer(s, &server{channel: ch})
+
+	log.Printf("GRPC server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+type server struct {
+	pb_checkout.UnimplementedCheckoutServer
+	channel *amqp.Channel
+}
+
+func (s *server) DoCheckOut(ctx context.Context, rq *pb_checkout.CheckOutRequest) (*pb_checkout.CheckOutResponse, error) {
+	messageName := "checkout.processed"
+
+	// Create a new span (child of the trace id) to inform the publishing of the message
+	tr := otel.Tracer("amqp")
+	amqpContext, messageSpan := tr.Start(ctx, fmt.Sprintf("AMQP - publish - %s", messageName))
+	defer messageSpan.End()
+
+	// Inject the context in the headers
+	headers := utils.InjectAMQPHeaders(amqpContext)
+	msg := amqp.Publishing{Headers: headers}
+	err := s.channel.Publish("exchange", messageName, false, false, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response := &pb_checkout.CheckOutResponse{TotalAmount: 1234}
+
+	// Log specific events for a span
+	span := trace.SpanFromContext(ctx)
+	span.AddEvent(fmt.Sprintf("response: %v", response))
+
+	return response, nil
 }
